6df701a3fa547ac6c98861f8452f1ec01f081ff0
Kevingallegos2003 <kevingallegos2003@gmail.com>
2025-02-22 19:20:58 -0800

    Trying to get animation to work

---
+++ lab5/Scenes/Gallery.js
@@ -0,0 +1,63 @@
+class Gallery extends Phaser.Scene {
+    constructor() {
+        super("GalleryScene");
+        this.my = {sprite: {}};
+    }
+    preload(){
+        this.load.spritesheet('Camp', 'Scenes/spritesheet.png',{
+            frameWidth: 1920,
+            frameHeight: 1080,
+        });
+    }
+    create(){
+        let my = this.my;
+        my.sprite = this.add.sprite(400, 300, 'Camp');
+        var animationConfig = {
+            key: "Camp",
+          
+            frames: [this.anims.generateFrameNumbers("Camp", { start: 0, end: 9 })],
+          
+            repeat: -1,
+            frameRate: null,
+        };
+          
+        this.anims.create(animationConfig);
+        my.sprite.play("Camp",true);
+        
+        var ruleGrammar = tracery.createGrammar({
+                
+            "move":["flock", "race", "glide", "dance", "flee", "lie"],
+            "bird":["swan", "heron", "sparrow", "swallow", "wren", "robin"],
+            "animals":["#bird#", "Turtle", "Banana Slug", "SilverFish","Panda"],
+            "agent":["cloud", "wave", "#bird#", "boat", "ship"],
+            "transVerb":["forget", "plant", "greet", "remember", "embrace", "feel", "love"],
+            "emotion":["sorrow", "gladness", "joy", "heartache", "love", "forgiveness", "grace"],
+            "substance":["#emotion#", "mist", "fog", "glass", "silver", "rain", "dew", "cloud", "virtue", "sun", "shadow", "gold", "light", "darkness"],
+            "adj":["fair", "bright", "splendid", "divine", "inseparable", "fine", "lazy", "grand", "slow", "quick", "graceful", "grave", "clear", "faint", "dreary"],
+            "doThing":["come", "move", "cry", "weep", "laugh", "dream"],
+            "verb":["fleck", "grace", "bless", "dapple", "touch", "caress", "smooth", "crown", "veil"],
+            "ground":["glen", "river", "vale", "sea", "meadow", "forest", "glade", "grass", "sky", "waves"],
+            "poeticAdj":["#substance#-#verb.ed#"],
+            "poeticDesc":["#poeticAdj#", "by #substance# #verb#'d", "#adj# with #substance#", "#verb.ed# with #substance#"],
+            "ah":["ah", "alas", "oh", "yet", "but", "and"],
+            "on":["on", "in", "above", "beneath", "under", "by", "within"],
+            "punctutation":[",", ":", " ", "!", ".", "?"],
+            "starters":["Once upon a time","Twas the #substance#", "Twas the #adj#", "Somewhere #on# the #ground#"],
+            "noun":["#ground#", "#agent#"],
+            "proto":["#starters# #move#ing #verb#fully #on# the #ground#, a #bird# felt #emotion# for the first time in there life#punctutation# "],
+            "proto2":["#ah# the #agent#s couldn't forgive its impatientness."],
+            "line":["#proto##proto2#"],
+            "poem":["#line##punctutation##line##punctutation##line##punctutation##line#."],
+            "origin":["#[sub:#noun#]poem#"]
+        });
+        var poem = ruleGrammar.flatten("#line#");
+        console.log(poem);
+        my.sprite.t = this.add.text(0, 0, poem);
+        
+        
+    }
+    update(){
+        
+    }
+    
+}
--- lab5/lab.js
+++ lab5/lab.js
@@ -1,32 +1,55 @@
-/**
- * @author Kate
- */
-
-var verbs = ["accept", "allow", "ask", "believe", "borrow", "break", "bring", "buy", "can”,”be able", "cancel", "change", "clean", "comb", "complain", "cough", "count", "cut", "dance", "draw", "drink", "drive", "eat", "explain", "fall", "fill", "find", "finish", "fit", "fix", "fly", "forget", "give", "go", "have", "hear", "hurt", "know", "learn", "leave", "listen", "live", "look", "lose", "make”,”do", "need", "open", "close", "shut", "organise", "pay", "play", "put", "rain", "read", "reply", "run", "say", "see", "sell", "send", "sign", "sing", "sit", "sleep", "smoke", "speak", "spell", "spend", "stand", "start”,”begin", "study", "succeed", "swim", "take", "talk", "teach", "tell", "think", "translate", "travel", "try", "turn off", "turn on", "type", "understand", "use", "wait", "wake up", "want", "watch", "work", "worry", "write"];
 
 
+"use strict"
+let canvasW = 900;
+let canvasH = 700;
+let canvasContainer;
+var centerHorz, centerVert;
 
-var ruleGrammar = tracery.createGrammar({
-		
-	"move":["flock", "race", "glide", "dance", "flee", "lie"],
-	"bird":["swan", "heron", "sparrow", "swallow", "wren", "robin"],
-	"agent":["cloud", "wave", "#bird#", "boat", "ship"],
-	"transVerb":["forget", "plant", "greet", "remember", "embrace", "feel", "love"],
-	"emotion":["sorrow", "gladness", "joy", "heartache", "love", "forgiveness", "grace"],
-	"substance":["#emotion#", "mist", "fog", "glass", "silver", "rain", "dew", "cloud", "virtue", "sun", "shadow", "gold", "light", "darkness"],
-	"adj":["fair", "bright", "splendid", "divine", "inseparable", "fine", "lazy", "grand", "slow", "quick", "graceful", "grave", "clear", "faint", "dreary"],
-	"doThing":["come", "move", "cry", "weep", "laugh", "dream"],
-	"verb":["fleck", "grace", "bless", "dapple", "touch", "caress", "smooth", "crown", "veil"],
-	"ground":["glen", "river", "vale", "sea", "meadow", "forest", "glade", "grass", "sky", "waves"],
-	"poeticAdj":["#substance#-#verb.ed#"],
-	"poeticDesc":["#poeticAdj#", "by #substance# #verb#'d", "#adj# with #substance#", "#verb.ed# with #substance#"],
-	"ah":["ah", "alas", "oh", "yet", "but", "and"],
-	"on":["on", "in", "above", "beneath", "under", "by"],
-	"punctutation":[",", ":", " ", "!", ".", "?"],
-	"noun":["#ground#", "#agent#"],
-	"line":["My #noun#, #poeticDesc#, my #adj# one", "More #adj# than #noun# #poeticDesc#", "#move.capitalize# with me #on# #poeticAdj# #ground#", "The #agent.s# #move#, #adj# and #adj#", "#poeticDesc.capitalize#, #poeticDesc#, #ah#, #poeticDesc#", "How #adj# is the #poeticDesc# #sub#", "#poeticDesc.capitalize# with #emotion#, #transVerb.s# the #noun#"],
-	"poem":["#line##punctutation##line##punctutation##line##punctutation##line#."],
-	"origin":["#[sub:#noun#]poem#"]
-});
-var poem = ruleGrammar.flatten("#poem#");
-console.log(poem);
+
+function preload(){
+
+}
+
+
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  canvasW = canvasContainer.width(); // Adjusted for drawing logic
+  canvasH = canvasContainer.height(); // Adjusted for drawing logic
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  //redrawCanvas(); // Redraw everything based on new size
+}
+
+// setup() function is called once when the program starts
+function setup() {
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), 100);
+  canvas.parent("canvas-container");
+  // resize canvas if the page is resized
+/*
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+  */
+
+  //lastFrame = performance.now(); // returned in milliseconds
+  //frameTime = 0;
+  imageMode(CENTER);
+}
+// game config
+let config = {
+    parent: 'canvas-container',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: false  // prevent pixel art from getting blurred when scaled
+    },
+    width: 1152,
+    height: 700,
+    fps: { forceSetTimeOut: true, target: 30 },
+    scene: [Gallery]
+}
+
+const game = new Phaser.Game(config);
